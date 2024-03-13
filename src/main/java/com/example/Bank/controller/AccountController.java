4495bc79072748b81a3d7e6bb081a38fda2dd194
Rajat Kumar (CW) <Rajat.Kumar1@jda.com>
2024-03-13 11:10:32 +0530

    dto

---
+++ src/main/java/com/example/Bank/controller/AccountController.java
@@ -0,0 +1,17 @@
+package com.example.Bank.controller;
+
+import com.example.Bank.Service.AccountService;
+import com.example.Bank.dto.UpdatePin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+    @PostMapping("/pin/update")
+//    public HttpStatus UpdatePin(@RequestBody UpdatePin updatePin){
+//
+//    }
+}
