d4c7894a54b39cc7eac5e05605fe9660c68c32bc
chinmayagarwal03 <chinmayagarwal4@gmail.com>
2024-03-13 10:34:42 +0530

    Account Service

--- src/main/java/com/example/Bank/Service/AccountService.java
+++ src/main/java/com/example/Bank/Service/AccountService.java
@@ -1,10 +1,7 @@
 package com.example.Bank.Service;
 import com.example.Bank.model.Account;
 
-public class AccountService {
-
     public interface AccountService {
         public Account AccountDetails(String accountNumber);
     }
-}
 
